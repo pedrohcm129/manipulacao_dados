f81efe72ba1929599a232fb2c393bf3c8a1a4732
Pedro Henrique Costa Menezes <92601935+pedrohcm129@users.noreply.github.com>
2023-11-01 20:32:00 -0300

    create manipulacao_dados.c

---
+++ manipulacao_dados.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+        char string[20] = "-1as4r33ary65fgd3";
+        char *pont = string;
+
+        printf("%s\n", string);
+        while(*pont) {
+                if (*pont >= 97 && *pont <= 122) {
+                        *pont = *pont - 32;
+                }
+
+                if (*pont >= 48 && *pont <= 57) {
+                        int valor = *pont - '0';
+                        int raiz_quadrada_inteira = sqrt(valor);
+                        *pont = raiz_quadrada_inteira + '0';
+                }
+                pont++;
+        }
+
+        printf("%s", string);
+        return 0;
+}
